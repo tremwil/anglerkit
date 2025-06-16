d3b6afacd4e42c2ba60ac4363a4e38e6af88aba1
tremwil <tremwil@gmail.com>
2025-06-16 18:42:22 +0100

    invert cfgs for std feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-#![cfg_attr(feature = "no_std", no_std)]
+#![cfg_attr(not(feature = "std"), no_std)]
 #![deny(unsafe_op_in_unsafe_fn)]
 
-#[cfg(feature = "no_std")]
+#[cfg(not(feature = "std"))]
 extern crate alloc as _liballoc;
-#[cfg(not(feature = "no_std"))]
+#[cfg(feature = "std")]
 use std as liballoc;
 
-#[cfg(feature = "no_std")]
+#[cfg(not(feature = "std"))]
 use _liballoc as liballoc;
 
 pub mod traits;
